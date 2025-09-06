046e3e1f3f3247bfcddcb32259eccfd382a5718c
MahammadWahab77 <gundluru.mahammadwahab@nxtwave.co.in>
2025-09-06 17:29:38 +0530

    Replace bcrypt with bcryptjs

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import multer from "multer";
 import fs from "fs";
 import path from "path";
